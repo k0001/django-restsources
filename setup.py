53d0111e2a63f49ec572790b6df81c5d87c400c5
Renzo Carbonara <gnuk0001@gmail.com>
2012-12-16 22:19:47 -0300

    Bump version to 0.1alpha3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-restsources",
-    version = "0.1alpha2",
+    version = "0.1alpha3",
     packages = find_packages(),
 
     zip_safe = True,
